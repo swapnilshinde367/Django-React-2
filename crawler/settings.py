aeaf67e45e1c96f9902677db58a8ae995ef91124
XCHANGING.MY\Swapnil.Shinde <swapnil.shinde@asia.xchanging.com>
2019-04-09 12:42:52 +0800

    Add allowed hosts

--- crawler/settings.py
+++ crawler/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h&tk_wwh2ui^cr2wpy0*ou6u(ieajl47g0gy#r(aa81i&2x6lj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [react-django-crawler.herokuapp.com, 127.0.0.1]
 
 
 # Application definition
